e293fe730bbc55ece3b09f5f5b9c785b0aae3999
mmminggg <meezee201@gmail.com>
2021-11-08 13:34:06 +0900

    로그인 페이지 완성

--- js/main.js
+++ js/main.js
@@ -132,5 +132,3 @@ spyEls.forEach(function (spyEl) {
     .addTo(new ScrollMagic.Controller())
 })
 
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear(); // 2021
